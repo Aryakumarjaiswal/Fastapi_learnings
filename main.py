aaa6f5c478b30322ed334b983c02bd7b5f01cbb0
Aryakumarjaiswal <86922289+Aryakumarjaiswal@users.noreply.github.com>
2024-11-12 18:39:50 +0530

    level 1

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-#Level 0
 from fastapi import FastAPI
 app=FastAPI()#initialise in variable 
 
 @app.get('/')
-def intro():
+def wish():
 
     return "Congratulations Aryan for your First Fastapi app"
-#In ternimal write
-#uvicorn file_name_without.py:initialised_variable --reload(--reload auto restarts server if any change occurs)
-#i.e uvicorn main:app --reload
+
+
+@app.get('/job/')
+def job_intro():
+    
+    return "I'm AI Developer in MaitriAI"
+
+
+Bag=["Apple","Banana","Mango","Chickoo"]
+
+@app.get('/bag')
+def show_item():
+    return Bag
+
+
+Bag=["Apple","Banana","Mango","Chickoo"]
+@app.get('/bag/{item_no}')
+def fruits(item_no):
+
+    
+    
+    if len(Bag)<int(item_no):
+        return "False"
+    return Bag[int(item_no)]
+
+
